2d869c4a2db0dc06df8aa80b981c62da8129104e
Noel Dawe <noel.dawe@gmail.com>
2013-03-09 15:39:53 +0100

    new hist example

--- overview/examples/hist.py
+++ overview/examples/hist.py
@@ -1,22 +1,11 @@
-from rootpy.plotting import Hist, Hist2D, Hist3D
+from ROOT import TH3D
+from array import array
 
-# create a 1D histogram
-h = Hist(10, 0, 1)
-
-# 2D and 3D histograms are handled in the same way
-# the constructor arguments are repetitions of #bins, left bound, right bound.
-h2d = Hist2D(10, 0, 1, 50, -40, 10, name='2d hist')
-h3d = Hist3D(3, -1, 4, 10, -1000, -200, 2, 0, 1, name='3d hist')
-
-# variable-width bins may be created by passing the bin edges directly:
-h1d_variable = Hist([1, 4, 10, 100])
-h2d_variable = Hist2D([2, 4, 7, 100, 200], [-100, -50, 0, 10, 20])
-h3d_variable = Hist3D([1, 3, 10], [20, 50, 100], [-10, -5, 10, 20])
-
-# variable-width and constant-width bins can be mixed:
-h2d_mixed = Hist2D([2, 10, 30], 10, 1, 5)
-
-# easily set visual attributes
-h.linecolor = 'blue'
-h.fillcolor = 'green'
-h.fillstyle = '/'
+# variable width bins
+hist3d = TH3D('3d', '3d', 3, array('d', [0, 3, 10, 100]),
+                          5, array('d', [2.3, 4.2, 5.8, 10, 20, 25.5]),
+                          2, array('d', [-100, 0, 20]))
+# ROOT is missing some constructors... (the following will not work)
+hist3d = TH3D('3d', '3d', 3, 0, 5,
+                          5, array('d', [2.3, 4.2, 5.8, 10, 20, 25.5]),
+                          2, array('d', [-100, 0, 20]))
---
+++ overview/examples/hist_rootpy.py
@@ -0,0 +1,6 @@
+from rootpy.plotting import Hist3D
+
+# variable width bins
+hist3d = Hist3D([0, 3, 10, 100], [2.3, 4.2, 5.8, 10, 20, 25.5], [-100, 0, 20])
+# easy to mix variable and fixed width bins with rootpy
+hist3d = Hist3D(3, 0, 5, [2.3, 4.2, 5.8, 10, 20, 25.5], [-100, 0, 20])
