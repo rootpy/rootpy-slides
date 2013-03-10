d88c301dfa2f01c22c3e3bf16465b573eda7b87f
Noel Dawe <noel.dawe@gmail.com>
2013-03-10 10:02:37 +0100

    updates

--- overview/examples/file.py
+++ overview/examples/file.py
@@ -2,6 +2,7 @@ from rootpy.testdata import get_file
 
 # use the test file shipped with rootpy
 with get_file() as f:
+    # access objects by name as properties of the current dir
     myhist = f.dimensions.hist2d
     # recursively walk through the file
     for path, dirs, objects in f.walk():
