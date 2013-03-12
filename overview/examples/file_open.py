e42274ab375b0a5ff5d0b58d4df1af0ba7a061d6
Noel Dawe <noel.dawe@gmail.com>
2013-03-12 17:46:02 +0100

    updates

--- overview/examples/file_open.py
+++ overview/examples/file_open.py
@@ -1,4 +1,8 @@
-# open a file with ROOT
+"""
+ROOT is unable to open the file of course and emits an error message but an
+exception is not raised at this point leading to (sometimes difficult to
+intepret) issues downstream:
+"""
 from ROOT import TFile
 
 myfile = TFile.Open("file_does_not_exist.root")
--- overview/examples/file_open_rootpy.py
+++ overview/examples/file_open_rootpy.py
@@ -1,4 +1,7 @@
-# open a file with rootpy
+"""
+rootpy routes ROOT messages through Python`s logging system and raises error
+messages as Python exceptions at the point of failure:
+"""
 from rootpy.io import root_open
 
 myfile = root_open("file_does_not_exist.root")
