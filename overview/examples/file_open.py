433f38c3e9ce36579079ebca30ad393ad09741c8
Noel Dawe <noel.dawe@gmail.com>
2013-03-13 13:05:53 +0100

    minor edits

--- overview/examples/file_open.py
+++ overview/examples/file_open.py
@@ -1,7 +1,7 @@
 """
 ROOT is unable to open the file of course and emits an error message but an
 exception is not raised at this point leading to (sometimes difficult to
-intepret) issues downstream:
+interpret) issues downstream:
 """
 from ROOT import TFile
 
