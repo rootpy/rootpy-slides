a15301e0bb02ef7b184fae11e6299a37845488f9
Noel Dawe <noel.dawe@gmail.com>
2016-09-19 23:17:16 +1000

    slide updates

--- overview/examples/tree_rootpy_with_model.py
+++ overview/examples/tree_rootpy_with_model.py
@@ -1,14 +1,16 @@
 # test
-from rootpy.tree import Tree
+from rootpy.tree import Tree, TreeModel
+from rootpy.tree import FloatCol, IntCol
 from rootpy.io import root_open
 from random import gauss
 
 f = root_open("test.root", "recreate")
 
-tree = Tree("test")
-tree.create_branches(
-    {'some_float': 'F',
-     'some_int': 'I'})
+class Model(TreeModel):
+    some_float = FloatCol()
+    some_int = IntCol()
+
+tree = Tree("test", model=Model)
 
 for i in xrange(10000):
     tree.some_float = gauss(.5, 1.)
